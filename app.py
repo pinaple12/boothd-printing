6ac516727b760e501c8cc0a6d1fc29fefcf681c4
lil-j <jake@krunchy.net>
2024-11-11 01:40:36 -0800

    change host back

--- app.py
+++ app.py
@@ -496,5 +496,5 @@ with camera_lock:
     #set_camera_preview_settings()
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0')
-    app.run(port=5001)
+    app.run(host='0.0.0.0')
+    # app.run(port=5001)
